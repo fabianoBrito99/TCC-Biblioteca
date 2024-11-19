0c54218c77f5912b49184bf7cf785bce7184c9bd
Fabiano Brito <fabiano99.fbs@gmail.com>
2024-11-19 17:40:59 -0300

    atualizações card livro

--- src/componentes/cardLivros/livro-categorias.tsx
+++ src/componentes/cardLivros/livro-categorias.tsx
@@ -1,90 +1,85 @@
-import { useEffect } from 'react';
-import Swiper from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import { useRouter } from 'next/navigation';
-import styles from './livroCategorias.module.css';
+import { useEffect } from "react";
+import Swiper from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { useRouter } from "next/navigation";
+import styles from "./livroCategorias.module.css";
 
 interface Livro {
-  id: string;
+  id_livro: string;
   nome_livro: string;
   foto_capa: string;
   autor: string;
-  categoria: string;
+  categoria_principal: string;
 }
 
+
 interface CategoriaSwiperProps {
-  categoria: string;
+  categoria_principal: string;
   livros: Livro[];
 }
 
-const CategoriaSwiper: React.FC<CategoriaSwiperProps> = ({ categoria, livros }) => {
-  const router = useRouter(); 
+const CategoriaSwiper: React.FC<CategoriaSwiperProps> = ({
+  categoria_principal,
+  livros,
+}) => {
+  const router = useRouter();
 
   useEffect(() => {
-    
-    // Inicializa o Swiper para cada categoria
-    new Swiper(`.mySwiper-${categoria}`, {
-      slidesPerView: 3,
-      spaceBetween: 10,
-      navigation: {
-        nextEl: `.mySwiper-${categoria} .custom-swiper-button-next`,
-        prevEl: `.mySwiper-${categoria} .custom-swiper-button-prev`,
-      },
-      pagination: {
-        el: `.mySwiper-${categoria} .custom-swiper-pagination`,
-        clickable: true,
-      },
-      breakpoints: {
-        140: {
-          slidesPerView: 1,
-          spaceBetween: 1,
+    if (livros.length > 0) {
+      const swiper = new Swiper(`.mySwiper-${categoria_principal}`, {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: `.mySwiper-${categoria_principal} .custom-swiper-button-next`,
+          prevEl: `.mySwiper-${categoria_principal} .custom-swiper-button-prev`,
         },
-        540: {
-          slidesPerView: 2,
-          spaceBetween: 1,
+        pagination: {
+          el: `.mySwiper-${categoria_principal} .custom-swiper-pagination`,
+          clickable: true,
         },
-        790: {
-          slidesPerView: 3,
-          spaceBetween: 1,
+        breakpoints: {
+          140: { slidesPerView: 1, spaceBetween: 1 },
+          540: { slidesPerView: 2, spaceBetween: 1 },
+          790: { slidesPerView: 3, spaceBetween: 1 },
+          1124: { slidesPerView: 4, spaceBetween: 1 },
+          1500: { slidesPerView: 5.5, spaceBetween: 0.5 },
         },
-        1124: {
-          slidesPerView: 4,
-          spaceBetween: 1,
-        },
-        1500: {
-          slidesPerView: 5.5,
-          spaceBetween: 0.5,
-        },
-      },
-    });
-  }, [categoria, livros]);
+      });
+      return () => swiper.destroy(); // Limpeza da instância do Swiper ao desmontar
+    }
+  }, [livros, categoria_principal]);
 
   const handleCardClick = (id: string) => {
-    router.push(`/livro/${id}`); // Redireciona para a página do livro
+    router.push(`/livro/${id}`);
   };
 
   return (
     <div className={styles.categoriaContainer}>
-      <h3 className={styles.categoria}>{categoria}</h3>
-      <div className={`swiper custom-swiper mySwiper-${categoria}`}>
+      <h3 className={styles.categoria}>{categoria_principal}</h3>
+      <div className={`swiper mySwiper-${categoria_principal}`}>
         <div className="swiper-wrapper">
           {livros.map((livro) => (
             <div
-              key={livro.id}
+              key={livro.id_livro}
               className="swiper-slide"
-              onClick={() => handleCardClick(livro.id)} // Adiciona o evento de clique
-              style={{ cursor: 'pointer' }} // Opcional: cursor para indicar que é clicável
+              onClick={() => handleCardClick(livro.id_livro)}
+              style={{ cursor: "pointer" }}
             >
               <div className={styles.cardLivro}>
-                <img src={livro.foto_capa} alt={livro.nome_livro} />
+                <img
+                  src={livro.foto_capa}
+                  alt={livro.nome_livro}
+                  className="livro-capa"
+                />
                 <h2>{livro.nome_livro}</h2>
-                <h4>Autor: {livro.autor}</h4>
+                <h4>{livro.autor}</h4>
               </div>
             </div>
           ))}
         </div>
+
         <div className="swiper-pagination custom-swiper-pagination"></div>
         <div className="custom-swiper-button-next"></div>
         <div className="custom-swiper-button-prev"></div>
