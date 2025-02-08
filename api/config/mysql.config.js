a53f4bce597dce8df1451a55fb56508e844a7e97
fabianoBrito99 <fabiano99.fbs@gmail.com>
2025-02-08 16:03:49 -0400

    atualizações notebook

--- api/config/mysql.config.js
+++ api/config/mysql.config.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 3306,
+  port: 3307,
   user: 'root',
   password: 'root',
   database: 'biblioteca_tcc'
